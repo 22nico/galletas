33b496ad3df0a20a1a19cfcf6d16013dd93f7238
22nico <141750044+22nico@users.noreply.github.com>
2023-08-10 10:55:25 -0300

    resabiendo archivos

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function quieresgalletas() {
+    if (confirm("¿quieres galletas?")) {
+        //esto se eejecuta si es true
+        alert("aquitienes tuds galletas ")
+    prompt("como estuvieron las galletas")
+    }
+    else{
+        //esto de ejecuta si es false 
+        alert("que lastima mas parra mi")
+    }
+}
